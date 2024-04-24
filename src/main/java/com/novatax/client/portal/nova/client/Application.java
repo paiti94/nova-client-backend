69cf1d9d878184130160c9d6b954fe6377341f91
paiti94 <paiti94@gmail.com>
2024-04-24 10:20:41 -0700

    Initial commit

---
+++ src/main/java/com/novatax/client/portal/nova/client/Application.java
@@ -0,0 +1,13 @@
+package com.novatax.client.portal.nova.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/novatax/client/portal/nova/client/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novatax.client.portal.nova.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
